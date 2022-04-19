5961603cb85599e0aafd6a19a6abf059c6e33ad0
Yulchiko <Yulchik.82@i.ua>
2022-04-19 23:05:53 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useReducer} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const reducer = (state, action) => {
+    const {type} = action;
+    switch (type) {
+        case 'inc1':
+            return {...state, count1: state.count1 + 1}
+        case 'inc2':
+            return {...state, count2: state.count2 + 1}
+        case 'inc3':
+            return {...state, count3: state.count3 + 1}
+        case 'des1':
+            return {...state, count1: state.count1 - 1}
+        case 'des2':
+            return {...state, count2: state.count2 - 1}
+        case 'des3':
+            return {...state, count3: state.count3 - 1}
+        case 'reset1':
+            return {...state, count1: 0}
+        case 'reset2':
+            return {...state, count2: 0}
+        case 'reset3':
+            return {...state, count3: 0}
+
+        default :
+            console.error('невідомий тип!');
+            return state;
+    }
 }
+
+const App = () => {
+    const [state, dispatch] = useReducer(reducer, {count1: 0, count2: 0, count3: 0})
+    return (
+        <div>
+            <div style={{display:'flex', justifyContent:'center'}}>
+                <div> Count #1: {state.count1}</div>
+                <button onClick={() => dispatch({type: 'inc1'})}>inc</button>
+                <button onClick={() => dispatch({type: 'des1'})}>des</button>
+                <button onClick={() => dispatch({type: 'reset1'})}>reset</button>
+                <div style={{marginLeft:'50px'}}> Count #2: {state.count2}</div>
+                <button onClick={() => dispatch({type: 'inc2'})}>inc</button>
+                <button onClick={() => dispatch({type: 'des2'})}>des</button>
+                <button onClick={() => dispatch({type: 'reset2'})}>reset</button>
+                <div style={{marginLeft:'50px'}}> Count #3: {state.count3}</div>
+                <button onClick={() => dispatch({type: 'inc3'})}>inc</button>
+                <button onClick={() => dispatch({type: 'des3'})}>des</button>
+                <button onClick={() => dispatch({type: 'reset3'})}>reset</button>
+            </div>
+        </div>
+    );
+};
 
 export default App;
